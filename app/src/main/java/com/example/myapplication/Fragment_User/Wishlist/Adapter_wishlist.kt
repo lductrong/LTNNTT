0d0984ac05324271bcb762744a1dc1c7a68c1c94
LeVuXuanAn <levuxuanan.work@gmail.com>
2024-12-06 18:23:40 +0700

    first commit

--- app/src/main/java/com/example/myapplication/Fragment_User/Wishlist/Adapter_wishlist.kt
+++ app/src/main/java/com/example/myapplication/Fragment_User/Wishlist/Adapter_wishlist.kt
@@ -11,20 +11,26 @@ import com.example.myapplication.Data.Model.Model_cart
 import com.example.myapplication.Data.Model.Model_product
 import com.example.myapplication.R
 
-class Adapter_wishlist(private val wishlistList: ArrayList<Model_product>) : RecyclerView.Adapter<Adapter_wishlist.CartViewHolder>() {
-    class CartViewHolder(view: View): RecyclerView.ViewHolder(view) {
+class Adapter_wishlist(private val wishlistList: ArrayList<Model_product>) :
+    RecyclerView.Adapter<Adapter_wishlist.CartViewHolder>() {
+    class CartViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val txt_nameproduct: TextView = view.findViewById(R.id.txt_nameproduct)
         val txt_price: TextView = view.findViewById(R.id.txt_price)
         val imageproduct: ImageView = view.findViewById(R.id.imageproduct)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter_wishlist.CartViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_wishlist, parent, false)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): Adapter_wishlist.CartViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_wishlist, parent, false)
 
         return CartViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: Adapter_wishlist.CartViewHolder, position: Int) {
+        val test = 0 //tester
         val cart = wishlistList[position]
         holder.txt_nameproduct.text = cart.tensp
         holder.txt_price.text = cart.gia.toString()
